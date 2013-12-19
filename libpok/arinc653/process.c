ae60b98d4fc2132f08718a4aea9fa05540832d7b
Maxim Malkov <malkov@ispras.ru>
2013-12-19 17:01:56 +0400

    STOP function should be implemented now

--- libpok/arinc653/process.c
+++ libpok/arinc653/process.c
@@ -239,14 +239,16 @@ void START (PROCESS_ID_TYPE   process_id,
 	DELAYED_START(process_id,0,return_code);
 }
 
-#ifndef POK_CONFIG_OPTIMIZE_FOR_GENERATED_CODE
 void STOP (PROCESS_ID_TYPE    process_id,
-						RETURN_CODE_TYPE *return_code )
+						RETURN_CODE_TYPE *RETURN_CODE )
 {
-	 (void) process_id;
-	 *return_code = NOT_AVAILABLE;
+	pok_ret_t core_ret = pok_thread_stop(process_id);
+	switch (core_ret) {
+	    MAP_ERROR(POK_ERRNO_OK, NO_ERROR);
+	    // XXX NO_ACTION case
+	    default: *RETURN_CODE = INVALID_PARAM;
+	}
 }
-#endif
 
 void DELAYED_START (PROCESS_ID_TYPE   process_id,
 				SYSTEM_TIME_TYPE  delay_time,
