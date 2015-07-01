d470809b85333c735afe33f77d949c6618b763bc
Kurban Mallachiev <mkurmag@gmail.com>
2015-07-01 19:04:11 +0300

    change kernel printf

--- kernel/libc/printf.c
+++ kernel/libc/printf.c
@@ -24,279 +24,272 @@
 
 static const char digits[] = "0123456789abcdef";
 
-#define INT_UNSIGNED    1
-#define INT_SIGNED      2
-
-#define BASE_HEX        16
-#define BASE_OCT        8
-
-#define MY_BUF_SIZE     8192
+#define BUF_SIZE 128
 
 /*
  * some types
  */
 
-struct		s_file
+struct s_file
 {
-   char		buff[MY_BUF_SIZE];
-   size_t	pos;
+    char      buff[BUF_SIZE];
+    size_t    pos;
 };
 
-union		u_arg
-{
-   unsigned long	value;
-   unsigned int	uint;
-   int            sint;
-   void*          ptr;
-};
-
-typedef int (*t_fmtfun)(union u_arg *arg, struct s_file *file, int flags);
-
-struct		s_format
-{
-   char     ch;
-   t_fmtfun	fun;
-   int      flags;
-};
+typedef void (*t_putc)(char val, void *out);
 
 /*
  * buffered I/O
  */
 
-static void	my_fflush(struct s_file *file)
+static void buf_flush(struct s_file *file)
 {
-   pok_cons_write (file->buff, file->pos);
-   file->pos = 0;
+    pok_cons_write (file->buff, file->pos);
+    file->pos = 0;
 }
 
-static struct s_file*	init_buffered_output(void)
+static struct s_file* init_buffered_output(void)
 {
-   static struct s_file	res;
+    static struct s_file res;
 
-   res.pos = 0;
-   return &res;
+    res.pos = 0;
+    return &res;
 }
 
-static void	my_putc(char c, struct s_file *file)
+static void buf_putc(char c, void *out)
 {
-   file->buff[file->pos++] = c;
+    struct s_file * file = out;
+    file->buff[file->pos++] = c;
 
-   if (file->pos == MY_BUF_SIZE)
-   {
-      my_fflush(file);
-   }
+    if (file->pos == BUF_SIZE)
+        buf_flush(file);
 }
 
-static void	close_buffered_output(struct s_file *file)
+static void close_buffered_output(struct s_file *file)
 {
-   my_fflush(file);
+    buf_flush(file);
 }
 
-/*
- * formatting functions
- */
-
-static int	conv(unsigned int n, int base, int dig[])
+static void print_num(t_putc putc,
+                      void *out,
+                      unsigned long long value, 
+                      unsigned base, 
+                      int pad,
+                      int neg,
+                      int pad_with_zero)
 {
-   int i;
+    unsigned char digit_str[32]; //64-bit number in octal base is 32 digits long
+    int size = 0;
+    do {
+        unsigned mod = value%base;
+        value /= base;
+        digit_str[size] = digits[mod];
+        size++;
 
-   i = 0;
+    } while (value);
 
-   while (n)
-   {
-      dig[i] = n % base;
-      ++i;
-      n /= base;
-   }
-   return i - 1;
+    if (pad_with_zero) {
+        if (neg) {
+            putc('-', out);
+            pad--;
+        }
+        while (size < pad--)
+            putc('0', out);
+
+    } else {
+        if (neg)
+            pad--;
+
+        while (size < pad--)
+            putc(' ', out);
+
+        if (neg)
+            putc('-', out);
+    }
+
+    while (size--)
+        putc(digit_str[size], out);
 }
 
-static int	my_printnbr_base (unsigned int   n,
-                              const char     base[],
-                              int            card,
-                              struct s_file  *file)
+static void print_float(t_putc putc,
+                        void *out,
+                        long double value, 
+                        int pad, 
+                        int precision, 
+                        int neg,
+                        int pad_with_zero)
 {
-   int   digits[96];
-   int   i;
-   int   count;
+    long long floor = value;
+    long double fractional = 0;
 
-   if (n == 0)
-   {
-      my_putc ('0', file);
-      return 1;
-   }
+    print_num(putc, out, floor, 10, pad-(precision+1), neg, pad_with_zero);
 
-   count = i = conv (n, card, digits);
+    putc('.', out);
 
-   for (; i >= 0; --i)
-   {
-      my_putc(base[digits[i]], file);
-   }
-
-   return count;
-}
-
-static int	print_int (union u_arg* value,
-                       struct s_file* file,
-                       int flags)
-{
-   int sh;
-
-   sh = 0;
-
-   if (value->sint == 0)
-   {
-      my_putc('0', file);
-      return 1;
-   }
-
-   if (flags == INT_SIGNED)
-   {
-      if (value->sint < 0)
-      {
-         my_putc('-', file);
-         value->uint = -value->sint;
-         sh = 1;
-      }
-      else
-      {
-         value->uint = value->sint;
-      }
-   }
-
-   return my_printnbr_base (value->uint, digits, 10, file) + sh;
-}
-
-static int	print_str (union u_arg* value, struct s_file* file, int flags)
-{
-   int   count;
-   char*	s;
-
-   count = 0;
-   s     = value->ptr;
-
-   (void) flags;
-
-   for (; *s; ++count, ++s)
-   {
-      my_putc(*s, file);
-   }
-
-   return count;
-}
-
-static int	print_char(union u_arg* value, struct s_file* file, int flags)
-{
-   char	c;
-
-   (void) flags;
-
-   c     = value->sint;
-
-   my_putc (c, file);
-
-   return 1;
-}
-
-static int	print_base(union u_arg* value, struct s_file* file, int flags)
-{
-   return my_printnbr_base (value->uint, digits, flags, file);
-}
-
-static const struct s_format formats[] =
-{
-   { 'd', print_int, INT_SIGNED },
-   { 'i', print_int, INT_SIGNED },
-   { 'u', print_int, INT_UNSIGNED },
-   { 's', print_str, 0 },
-   { 'c', print_char, 0 },
-   { 'o', print_base, BASE_OCT },
-   { 'x', print_base, BASE_HEX },
-   { 0, NULL, 0 }
-};
-
-static int special_char (char fmt,
-                         union u_arg* value,
-                         struct s_file* file)
-{
-   int i;
-
-   for (i = 0; formats[i].fun; ++i)
-   {
-      if (formats[i].ch == fmt)
-      {
-         break;
-      }
-   }
-
-   if (formats[i].fun)
-   {
-      return formats[i].fun(value, file, formats[i].flags);
-   }
-   else
-   {
-      if (fmt != '%')
-      {
-         my_putc('%', file);
-      }
-
-      my_putc(fmt, file);
-
-      return 1 + (fmt != '%');
-   }
+    fractional = (value - floor);
+    for (int i=0; i<precision; i++)
+        fractional *= 10;
+    print_num(putc, out, fractional, 10, precision, 0, 1);
 }
 
 /*
  * finally, printf
  */
-
-int vprintf (const char* format, va_list args)
+//XXX for now precision is used only for floating points
+const char * handle_fmt(t_putc putc, void * out, const char* format, va_list args)
 {
-   struct s_file* file;
-   union u_arg	   arg;
-   int            count;
+    unsigned l = 0;
+    int pad = 0;
+    int pad_with_zero = 0; // if 1 than add zeros for padding
+    int precision = 0;
+    int was_dot = 0; //if 1 than there were '.' in fmt before
+    while (*format) {
+        switch (*format) {
+            case '0':
+                if (was_dot)
+                    precision *= 10;
+                else if (pad == 0)
+                    pad_with_zero = 1;
+                else
+                    pad *= 10;
+                format++;
+                break;
+            case '1'...'9':
+                if (was_dot)
+                    precision = *format - '0' + precision*10;
+                else
+                    pad = *format - '0' + pad*10;
+                format++;
+                break;
+            case 'l':
+                l++;
+                format++;
+                break;
+           case '.':
+                was_dot = 1;
+                format++;
+                break;
+           case 's':
+                {
+                    //TODO implement l!=0 case
+                    const char *s = va_arg(args, const char *);
+                    if (!s)
+                        s = (const char *)"(null)";
+                    int len = strlen(s);
 
-   count = 0;
+                    if (precision != 0 && precision<len)
+                        len = precision;
 
-   for (file = init_buffered_output();
-         *format;
-         format += (*format == '%' ? 2 : 1))
-   {
-      if (*format == '%')
-      {
-         if (!*(format + 1))
-         {
-            break;
-         }
+                    while (len < pad--)
+                        putc(' ', out);
+                    while (*s && len-- ) 
+                        putc(*s++, out);
+                    return ++format;
+                }
+           case 'p':
+                putc('0', out);
+                putc('x', out);
+           case 'x':
+                {
+                    long long value;
 
-         if (*(format + 1) != '%')
-         {
-            arg.value = va_arg(args, unsigned long);
-         }
+                    if (l == 2)
+                        value = va_arg(args, unsigned long long);
+                    else if (l == 1)
+                        value = va_arg(args, unsigned long);
+                    else 
+                        value = va_arg(args, unsigned );
 
-         count += special_char(*(format + 1), &arg, file);
-      }
-      else
-      {
-         my_putc(*format, file);
-         ++count;
-      }
-   }
+                    print_num(putc, out, value, 16, pad, 0, pad_with_zero);
+                    return ++format;
+                }
+           case 'd':
+           case 'i':
+                {
+                    long long value;
+                    if (l == 2)
+                        value = va_arg(args, long long);
+                    else if (l == 1)
+                        value = va_arg(args, long);
+                    else 
+                        value = va_arg(args, int);
+                    int neg = value < 0;
+                    if (neg)
+                        value = -value;
+                    print_num(putc, out, value, 10, pad, neg, pad_with_zero);
+                    return ++format;
+                }
+            case 'u':
+                {
+                    long long value;
+                    if (l == 2)
+                        value = va_arg(args, unsigned long long);
+                    else if (l == 1)
+                        value = va_arg(args, unsigned long);
+                    else 
+                        value = va_arg(args, unsigned );
 
-   close_buffered_output(file);
-   return count;
+                    print_num(putc, out, value, 10, pad, 0, pad_with_zero);
+                    return ++format;
+                }
+            case 'f':
+                {
+                    long double value;
+                    if (l == 2)
+                        value = va_arg(args, long double);
+                    else
+                        value = va_arg(args, double);
+                    int neg = value < 0;
+                    if (neg)
+                        value = -value;
+                    if (precision == 0)
+                        precision = 6;
+                    print_float(putc, out, value, pad, precision, neg, pad_with_zero);
+                    return ++format;
+                }
+            case 'c':
+                //TODO implement l!=0 case
+                putc(va_arg(args, unsigned ), out);
+                return ++format;
+            case '%':
+                putc('%', out);
+                return ++format;
+            case 0:
+                return format;
+            default:
+                putc('%', out);
+                putc(*format, out);
+                return ++format;
+        }
+    }
+    return format;
 }
 
-int printf (const char *format, ...)
+static void vprintf(t_putc putc, void *out, const char* format, va_list args)
 {
-   int      res;
-   va_list  args;
-
-   va_start (args, format);
-   res = vprintf (format, args);
-   va_end (args);
-
-   return res;
+    while(*format) {
+        if (*format == '%') {
+            format++;
+            format = handle_fmt(putc, out, format, args);
+        }
+        else
+            putc(*format++, out);
+    }
 }
+
+//Why no void?!
+int printf(const char *format, ...)
+{
+    va_list args;
+    struct s_file* out_file = init_buffered_output();
+
+    va_start(args, format);
+    vprintf (buf_putc, out_file, format, args);
+    va_end(args);
+
+    close_buffered_output(out_file);
+    return 0;
+}
+
 
 #endif
