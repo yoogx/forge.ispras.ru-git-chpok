cbf8d47d9e92830f07db54cb3ae8e9b891301cec
Kurban Mallachiev <mkurmag@gmail.com>
2015-07-21 15:25:44 +0300

    fix kernel warnings for x86. Add hexdump

--- kernel/core/debug.c
+++ kernel/core/debug.c
@@ -56,7 +56,7 @@ void pok_debug_print_current_state ()
    printf ("Current thread       : %lu\n", (unsigned long) POK_CURRENT_PARTITION.current_thread);
    printf ("Prev current thread  : %lu\n", (unsigned long) POK_CURRENT_PARTITION.prev_thread);
    printf ("Main thread          : %u\n", POK_CURRENT_PARTITION.thread_main);
-   printf ("Main thread entry    : 0x%x\n", POK_CURRENT_PARTITION.thread_main_entry);
+   printf ("Main thread entry    : 0x%lx\n", (unsigned long) POK_CURRENT_PARTITION.thread_main_entry);
    printf ("Partition threads sp :");
    for (i = POK_CURRENT_PARTITION.thread_index_low ; i < POK_CURRENT_PARTITION.thread_index_low + POK_CURRENT_PARTITION.thread_index ; i++)
    {
@@ -89,6 +89,51 @@ void pok_fatal (const char* message)
   pok_arch_idle ();
 
   __builtin_unreachable();
+}
+
+void hexdump (const void *addr, int len)
+{
+    int i;
+    unsigned char buff[17];
+    const unsigned char *pc = (const unsigned char*)addr;
+
+    if (len == 0) {
+        printf("Len is zero\n");
+        return;
+    }
+
+    // Process every byte in the data.
+    for (i = 0; i < len; i++) {
+        // Multiple of 16 means new line (with line offset).
+
+        if ((i % 16) == 0) {
+            // Just don't print ASCII for the zeroth line.
+            if (i != 0)
+                printf("  %s\n", buff);
+
+            // Output the offset.
+            printf("  %04x ", i);
+        }
+
+        // Now the hex code for the specific character.
+        printf(" %02x", pc[i]);
+
+        // And store a printable ASCII character for later.
+        if ((pc[i] < 0x20) || (pc[i] > 0x7e))
+            buff[i % 16] = '.';
+        else
+            buff[i % 16] = pc[i];
+        buff[(i % 16) + 1] = '\0';
+    }
+
+    // Pad out last line if not exactly 16 characters.
+    while ((i % 16) != 0) {
+        printf("   ");
+        i++;
+    }
+
+    // And print the final ASCII bit.
+    printf("  %s\n", buff);
 }
 
 #endif /* POK_CONFIG_NEEDS_DEBUG */
