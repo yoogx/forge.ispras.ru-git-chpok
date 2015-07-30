c635b6a435f276c05f1201e603c3f9c93ff82db6
Alexander <emelenkoalex@yandex.ru>
2015-07-30 13:09:49 +0300

    Replacing tabulations with spaces in console.c,readline.c

--- kernel/libc/monitor.c
+++ kernel/libc/monitor.c
@@ -7,7 +7,7 @@
 
 
 #define NCOMMANDS 8 //Number of commands, change it if you want to 
-		            //add a new command.
+                    //add a new command.
 
 /*
  *  Don't use '[' in your command!
@@ -49,14 +49,14 @@ int exit_from_monitor(int argc, char **argv); //exit from monitor
 int info_partition(int argc,char ** argv);
 
 struct Command {
-	const char *name;
+    const char *name;
     const char *argc;
-	const char *desc;
-	int (*func)(int argc, char** argv);
+    const char *desc;
+    int (*func)(int argc, char** argv);
 };
 
 static struct Command commands[] = {
-	{ "help", "" , "Display all list of commands", mon_help },
+    { "help", "" , "Display all list of commands", mon_help },
     { "help_about" , "/command/" , "Display descriptions of this command", help_about},
     {"ps", "" ,"Display list of partition",print_partition},
     {"info_partition", "/N/" ,"Display information about partition N",info_partition},
@@ -77,12 +77,12 @@ mon_help(int argc, char **argv)
         printf("Too many arguments for help!\n");
         return 0;
     }
-	int i;
-	(void) argc;
-	(void) argv;
-	for (i = 0; i < NCOMMANDS; i++)
-		printf("%s %s- %s\n", commands[i].name,commands[i].argc,commands[i].desc);
-	return 0;
+    int i;
+    (void) argc;
+    (void) argv;
+    for (i = 0; i < NCOMMANDS; i++)
+        printf("%s %s- %s\n", commands[i].name,commands[i].argc,commands[i].desc);
+    return 0;
 }
 
 int 
@@ -149,29 +149,29 @@ int info_partition(int argc,char **argv){
     printf("Info about partition #%d\n",number);
     printf("is_paused = %d\n",pok_partitions[number].is_paused);
     printf("base_addr = 0x%lx\n",pok_partitions[number].base_addr);     
-    printf("base_vaddr = 0x%lx\n",pok_partitions[number].base_vaddr);   	
-    printf("size = 0x%lx\n",pok_partitions[number].size);  	
-    printf("name = %s\n",pok_partitions[number].name);    	
-    printf("nthreads = %lu\n",pok_partitions[number].nthreads);    	
-    printf("priority = 0x%x\n",pok_partitions[number].priority);  	
-    printf("period = %lu\n",pok_partitions[number].period);   	
-    printf("activation = %llu\n",pok_partitions[number].activation);     	
-    printf("prev_thread = %lu\n",pok_partitions[number].prev_thread); 	
-    printf("current_thread = %lu\n",pok_partitions[number].current_thread);   	
-    printf("thread_index_low = %lu\n",pok_partitions[number].thread_index_low);   	
-    printf("thread_index_high = %lu\n",pok_partitions[number].thread_index_high);    	
-    printf("thread_index = %lu\n",pok_partitions[number].thread_index);  	
+    printf("base_vaddr = 0x%lx\n",pok_partitions[number].base_vaddr);       
+    printf("size = 0x%lx\n",pok_partitions[number].size);   
+    printf("name = %s\n",pok_partitions[number].name);      
+    printf("nthreads = %lu\n",pok_partitions[number].nthreads);     
+    printf("priority = 0x%x\n",pok_partitions[number].priority);    
+    printf("period = %lu\n",pok_partitions[number].period);     
+    printf("activation = %llu\n",pok_partitions[number].activation);        
+    printf("prev_thread = %lu\n",pok_partitions[number].prev_thread);   
+    printf("current_thread = %lu\n",pok_partitions[number].current_thread);     
+    printf("thread_index_low = %lu\n",pok_partitions[number].thread_index_low);     
+    printf("thread_index_high = %lu\n",pok_partitions[number].thread_index_high);       
+    printf("thread_index = %lu\n",pok_partitions[number].thread_index);     
 #if defined(POK_NEEDS_LOCKOBJECTS) || defined(POK_NEEDS_ERROR_HANDLING)
-    printf("lockobj_index_low = 0x%x\n",pok_partitions[number].lockobj_index_low);   	
-    printf("lockobj_index_high = 0x%x\n",pok_partitions[number].lockobj_index_high);    	
-    printf("nlockobjs = 0x%x\n",pok_partitions[number].nlockobjs);    	
+    printf("lockobj_index_low = 0x%x\n",pok_partitions[number].lockobj_index_low);      
+    printf("lockobj_index_high = 0x%x\n",pok_partitions[number].lockobj_index_high);        
+    printf("nlockobjs = 0x%x\n",pok_partitions[number].nlockobjs);      
 #endif
-    printf("thread_main = %d\n",pok_partitions[number].thread_main);	
+    printf("thread_main = %d\n",pok_partitions[number].thread_main);    
 #ifdef POK_NEEDS_IO
-    //printf("io_min = %d\n",pok_partitions[number].io_min);    	
-    //printf("io_max = %d\n",pok_partitions[number].io_max);    	
+    //printf("io_min = %d\n",pok_partitions[number].io_min);        
+    //printf("io_max = %d\n",pok_partitions[number].io_max);        
 #endif
-    //printf("lock_level = %d\n",pok_partitions[number].lock_level);   uint32 	
+    //printf("lock_level = %d\n",pok_partitions[number].lock_level);   uint32   
     printf("\n\n");
 
     
@@ -254,8 +254,8 @@ restart_N(int argc, char **argv){
 int 
 exit_from_monitor(int argc, char **argv){
 
-	(void) argc;
-	(void) argv;
+    (void) argc;
+    (void) argv;
     want_to_exit=TRUE;
     return 0;
 
@@ -274,68 +274,68 @@ exit_from_monitor(int argc, char **argv){
 static int
 runcmd(char *buf) 
 {
-	int argc;
-	char *argv[MAXARGS];
-	int i;
+    int argc;
+    char *argv[MAXARGS];
+    int i;
 
-	// Parse the command buffer into whitespace-separated arguments
-	argc = 0;
-	argv[argc] = 0;
-	while (1) {
-		// gobble whitespace
-		while (*buf && strchr(WHITESPACE, *buf))
-			*buf++ = 0;
-		if (*buf == 0)
-			break;
+    // Parse the command buffer into whitespace-separated arguments
+    argc = 0;
+    argv[argc] = 0;
+    while (1) {
+        // gobble whitespace
+        while (*buf && strchr(WHITESPACE, *buf))
+            *buf++ = 0;
+        if (*buf == 0)
+            break;
 
-		// save and scan past next arg
-		if (argc == MAXARGS-1) {
-			printf("Too many arguments (max %d)\n", MAXARGS);
-			return 0;
-		}
-		argv[argc++] = buf;
-		while (*buf && !strchr(WHITESPACE, *buf))
-			buf++;
-	}
-	argv[argc] = 0;
+        // save and scan past next arg
+        if (argc == MAXARGS-1) {
+            printf("Too many arguments (max %d)\n", MAXARGS);
+            return 0;
+        }
+        argv[argc++] = buf;
+        while (*buf && !strchr(WHITESPACE, *buf))
+            buf++;
+    }
+    argv[argc] = 0;
 
-	// Lookup and invoke the command
-	if (argc == 0)
-		return 0;
-	for (i = 0; i < NCOMMANDS; i++) {
-		if (strcmp(argv[0], commands[i].name) == 0)
-			return commands[i].func(argc, argv);
-	}
-	printf("Unknown command '%s'\n", argv[0]);
-	return 0;
+    // Lookup and invoke the command
+    if (argc == 0)
+        return 0;
+    for (i = 0; i < NCOMMANDS; i++) {
+        if (strcmp(argv[0], commands[i].name) == 0)
+            return commands[i].func(argc, argv);
+    }
+    printf("Unknown command '%s'\n", argv[0]);
+    return 0;
 }
 
 void
 monitor()
 {
-	char *buf;
+    char *buf;
     want_to_exit=FALSE;
-	printf("Welcome to the monitor!\n");
-	printf("Type 'help' for a list of commands.\n");
+    printf("Welcome to the monitor!\n");
+    printf("Type 'help' for a list of commands.\n");
 
 
-	while (!want_to_exit) {
-		buf = readline("K> ");
-		if (buf != NULL)
-			if (runcmd(buf) < 0)
-				break;
-        	
+    while (!want_to_exit) {
+        buf = readline("K> ");
+        if (buf != NULL)
+            if (runcmd(buf) < 0)
+                break;
+            
     }
 }
 
 
 void pok_monitor_thread(void)
 {
-	printf("pok_monitor_thread\n");		
+    printf("pok_monitor_thread\n");     
        pok_arch_preempt_enable(); //Initialize interrupts   
     for (;;) {
         if (data_to_read() == 1) {
-            pok_arch_preempt_disable();			
+            pok_arch_preempt_disable();         
             monitor();
             pok_arch_preempt_enable();        
         }
