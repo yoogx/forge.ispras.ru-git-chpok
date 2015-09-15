b3beb26fb1afd46e71f00b05a1a1f748d8344b32
Алексей Овчаров <aovch@ispras.ru>
2015-09-15 18:27:47 +0300

    fix for 36-bit address; added support for qemu >= 2.3.0

--- boards/e500v2/cons.h
+++ boards/e500v2/cons.h
@@ -19,7 +19,7 @@
 #define __POK_CONS_H__
 
 #define CCSRBAR_BASE         0xE0000000ULL
-#define CCSRBAR_BASE_PHYS    0xE0000000ULL
+#define CCSRBAR_BASE_PHYS    0xFE0000000ULL
 
 int         pok_cons_init(void);
 
