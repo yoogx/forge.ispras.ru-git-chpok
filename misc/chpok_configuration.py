f9a17beffdc9230f906b46bf04241b049137b7d7
Алексей Овчаров <aovch@ispras.ru>
2015-11-11 19:13:11 +0300

    got rid of config.c; moved its contents to partition's deployment.c

--- misc/chpok_configuration.py
+++ misc/chpok_configuration.py
@@ -880,6 +880,13 @@ def write_partition_deployment_c(conf, partition_idx, f):
     
     part = conf.partitions[partition_idx]
     
+    p("#include <config.h>")
+    p("#include <middleware/buffer.h>")
+    p("#include <middleware/blackboard.h>")
+    p("#include <arinc653/arincutils.h>")
+    p("#include <arinc653/event.h>")
+    p("#include <arinc653/semaphore.h>")
+    
     p("unsigned pok_config_nb_buffers = %d;" % part.num_arinc653_buffers)
     p("unsigned pok_config_buffer_data_size = %d;" % part.buffer_data_size)
     
@@ -892,3 +899,25 @@ def write_partition_deployment_c(conf, partition_idx, f):
     p("unsigned pok_config_nb_events = %d;" % part.num_arinc653_events)
     
     p("unsigned pok_config_nb_threads = %d;" % part.get_needed_threads())
+    
+    p("enum {")
+    p("tmp_pok_config_nb_buffers = %d," % part.num_arinc653_buffers)
+    p("tmp_pok_config_buffer_data_size = %d," % part.buffer_data_size)
+    p("tmp_pok_config_nb_blackboards = %d," % part.num_arinc653_blackboards)
+    p("tmp_pok_config_blackboard_data_size = %d," % part.blackboard_data_size)
+    p("tmp_pok_config_arinc653_nb_semaphores = %d," % part.num_arinc653_semaphores)
+    p("tmp_pok_config_arinc653_nb_events = %d," % part.num_arinc653_events)
+    p("tmp_pok_config_nb_threads = %d," % part.get_needed_threads())
+    p("};")
+    
+    p("pok_buffer_t pok_buffers[tmp_pok_config_nb_buffers];")
+    p("char pok_buffers_data[tmp_pok_config_buffer_data_size];")
+    
+    p("pok_blackboard_t pok_blackboards[tmp_pok_config_nb_blackboards];")
+    p("char pok_blackboards_data[tmp_pok_config_blackboard_data_size];")
+    
+    p("pok_arinc653_semaphore_layer_t pok_arinc653_semaphores_layers[tmp_pok_config_arinc653_nb_semaphores];")
+    
+    p("pok_arinc653_event_layer_t pok_arinc653_events_layers[tmp_pok_config_arinc653_nb_events];")
+    
+    p("ARINC_ATTRIBUTE arinc_process_attribute[tmp_pok_config_nb_threads];")
