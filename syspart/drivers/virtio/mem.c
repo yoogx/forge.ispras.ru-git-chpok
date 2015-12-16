3639628aaf3287f398af0502b32bbfc1fc03f7f3
Kurban Mallachiev <mallachiev@ispras.ru>
2015-12-16 20:11:49 +0300

    now allocated memory aligned by physical address

--- syspart/drivers/virtio/mem.c
+++ syspart/drivers/virtio/mem.c
@@ -1,7 +1,8 @@
 #include <types.h>
 #include <stdio.h>
+#include <memory.h>
 
-#define MEMSIZE 0x8000 //should be enough for virtio
+#define MEMSIZE 0xa000 //should be enough for virtio
 
 char start[MEMSIZE];
 //static void * start = (void *)0x4000000;
@@ -24,10 +25,33 @@ void * driver_mem_alloc (size_t sz)
     return res;
 }
 
+
+void * driver_mem_alloc_phys_aligned(size_t sz)
+{
+    char *res;
+
+    if (!end)
+        end = start;
+
+    res = (char *)(((unsigned) end + 4095) & ~4095);
+    uintptr_t phys = pok_virt_to_phys(res);
+    uintptr_t phys_aligned = ((uintptr_t) phys + 4095) & ~4095;
+    uintptr_t diff = phys_aligned - phys;
+
+    res += diff;
+    end = res + sz;
+    if ((unsigned)end > (unsigned)start + MEMSIZE) {
+        printf("Needs more memory!\n");
+        return NULL;
+    }
+    //printf("allocating %p\n", res);
+    return res;
+}
+
 void * driver_mem_alloc_aligned(size_t mem_size, size_t alignment) 
 {
     if (alignment == 4096)
-        return driver_mem_alloc(mem_size);
+        return driver_mem_alloc_phys_aligned(mem_size);
     else
         printf("Unsuported alignment");
         return NULL;
