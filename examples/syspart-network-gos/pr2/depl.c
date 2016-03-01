3e74a60dbcf6f6ff7fea4383c29b62d373f8e506
Kurban Mallachiev <mallachiev@ispras.ru>
2016-03-01 17:03:24 +0300

    add new example

---
+++ examples/syspart-network-gos/pr2/depl.c
@@ -0,0 +1,62 @@
+#include <sysconfig.h>
+
+enum {
+ overhead_p3041 = 0,
+ overhead_virtio = 10,
+ overhead_udp = 42,
+};
+
+static struct {
+    MESSAGE_SIZE_TYPE message_size;
+    pok_bool_t busy;
+    char data[overhead_p3041 + overhead_udp + 64];
+} sp_0_data;
+
+unsigned sysconfig_samples_nb = 1;
+sample_t samples[] = {
+    {
+        .max_message_size = 64,
+        .data = (void *) &sp_0_data,
+    },
+};
+unsigned sysconfig_queues_nb = 0;
+queue_t queues[] = {};
+
+//pok_device_t devices[] = {
+//    {
+//        .driver = DRIVER_VIRTIO,
+//        .kind = ETHERNET,
+//        .ethernet_data = {
+//            .ip = 0xa0000001
+//        }
+//    },
+//}
+
+#define IP_ADDR(a,b,c,d) ((uint32_t)((a) & 0xff) << 24) | \
+                         ((uint32_t)((b) & 0xff) << 16) | \
+                         ((uint32_t)((c) & 0xff) << 8)  | \
+                          (uint32_t)((d) & 0xff)
+
+const uint32_t pok_network_ip_address = IP_ADDR(192, 168, 160, 200);
+
+unsigned sysconfig_links_nb = 1;
+link_t links[] = {
+    {
+        .device_id = 0,
+        .buffer_idx = 0,
+        .linked_port_info = {
+            .kind = POK_PORT_KIND_SAMPLING,
+            .direction = DESTINATION,
+            .name = "SP2",
+            .sampling_data = {
+                .max_message_size = 64,
+            },
+        },
+
+        .protocol = UDP,
+        .udp_data = { //destination module ip
+            .ip = IP_ADDR(192, 168, 160, 99),
+            .port = 10000,
+        }
+    },
+};
