106e74ae1fc781c77f6fcd37397ff76f7e764ca8
Alexander Emelenko <emelenko@ispras.ru>
2016-06-21 17:05:19 +0300

    Fix for comment in GDB command file. (added '#')

--- misc/template_generation.py
+++ misc/template_generation.py
@@ -61,7 +61,7 @@ generate_title_c = """/*
  */
 """
 
-generate_title_c_no_track = '/* GENERATED! DO NOT MODIFY! */\n'
+generate_title_c_no_track = '#/* GENERATED! DO NOT MODIFY! */\n'
 
 def CopyWithTitle(target, source, env):
     """
