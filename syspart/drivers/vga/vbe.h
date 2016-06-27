dcbc352a728f0b833b42ff7f711716d20f47fbc5
Kurban Mallachiev <mallachiev@ispras.ru>
2016-06-27 21:11:55 +0300

    add licence headers

--- syspart/drivers/vga/vbe.h
+++ syspart/drivers/vga/vbe.h
@@ -1,3 +1,18 @@
+/*
+ * Institute for System Programming of the Russian Academy of Sciences
+ * Copyright (C) 2016 ISPRAS
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, Version 3.
+ *
+ * This program is distributed in the hope # that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License version 3 for more details.
+ */
+
 #ifndef __VBE_H__
 #define __VBE_H__
 
@@ -7,17 +22,18 @@
 #else
 #define VBE_DISPI_IOPORT_DATA 0x01CF
 #endif
+
 enum {
-    VBE_DISPI_INDEX_ID, // (0)
-    VBE_DISPI_INDEX_XRES, // (1)
-    VBE_DISPI_INDEX_YRES, // (2)
-    VBE_DISPI_INDEX_BPP, // (3)
-    VBE_DISPI_INDEX_ENABLE, // (4)
-    VBE_DISPI_INDEX_BANK, // (5)
-    VBE_DISPI_INDEX_VIRT_WIDTH, // (6)
-    VBE_DISPI_INDEX_VIRT_HEIGHT, // (7)
-    VBE_DISPI_INDEX_X_OFFSET, // (8)
-    VBE_DISPI_INDEX_Y_OFFSET, // (9)
+    VBE_DISPI_INDEX_ID,
+    VBE_DISPI_INDEX_XRES,
+    VBE_DISPI_INDEX_YRES,
+    VBE_DISPI_INDEX_BPP,
+    VBE_DISPI_INDEX_ENABLE,
+    VBE_DISPI_INDEX_BANK,
+    VBE_DISPI_INDEX_VIRT_WIDTH,
+    VBE_DISPI_INDEX_VIRT_HEIGHT,
+    VBE_DISPI_INDEX_X_OFFSET,
+    VBE_DISPI_INDEX_Y_OFFSET,
 };
 
 enum {
