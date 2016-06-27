5722dacf0f825b5c22eaea1b5e154f62dd78a50e
Kurban Mallachiev <mallachiev@ispras.ru>
2016-06-27 21:06:20 +0300

    minor fix: change use define instead hardcoded value

--- syspart/drivers/pci/vga.c
+++ syspart/drivers/pci/vga.c
@@ -9,20 +9,20 @@
 void vga_draw(void);
 
 struct gimp_image {
-  unsigned int 	 width;
-  unsigned int 	 height;
-  unsigned int 	 bytes_per_pixel; /* 2:RGB16, 3:RGB, 4:RGBA */ 
-  unsigned char	 pixel_data[];
+  unsigned int  width;
+  unsigned int  height;
+  unsigned int  bytes_per_pixel; /* 2:RGB16, 3:RGB, 4:RGBA */
+  unsigned char pixel_data[];
 };
 extern const struct gimp_image gimp_image;
 
 void vbe_write(struct pci_dev *dev, uint16_t reg, uint16_t val)
 {
-    (void) dev;
     iowrite16(reg, (void *)pci_convert_legacy_port(dev, VBE_DISPI_IOPORT_INDEX));
     iowrite16(val, (void *)pci_convert_legacy_port(dev, VBE_DISPI_IOPORT_DATA));
 }
 
+#define VGA_PAS 0x20 //palette address source. val 1 enables display.
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HIGHT 600
@@ -33,7 +33,7 @@ void vga_init()
 
     pci_get_dev_by_bdf(0, 1, 0, &vga_dev);
 
-    printf("vga bios[1] 0x%x\n", ioread8((uint8_t *)vga_dev.resources[6].addr));
+    printf("vga bios[0] 0x%x\n", ioread8((uint8_t *)vga_dev.resources[6].addr));
     printf("vga bios[1] 0x%x\n", ioread8((uint8_t *)vga_dev.resources[6].addr + 1));
 
     vbe_write(&vga_dev, VBE_DISPI_INDEX_ENABLE, 0);
@@ -43,7 +43,7 @@ void vga_init()
     vbe_write(&vga_dev, VBE_DISPI_INDEX_ENABLE, 1);
 
     //TODO this is mmio bar
-    iowrite8(0x20, (uint8_t *) pci_convert_legacy_port(&vga_dev, 0x3c0)); // PAS
+    iowrite8(VGA_PAS, (uint8_t *) pci_convert_legacy_port(&vga_dev, 0x3c0));
 
     vbe_write(&vga_dev, VBE_DISPI_INDEX_VIRT_WIDTH, SCREEN_WIDTH);
 
@@ -73,6 +73,7 @@ void vga_draw(void)
         }
     }
 }
+
 void vga_set_y_offset(int offset){
     vbe_write(&vga_dev, VBE_DISPI_INDEX_Y_OFFSET, offset);
 }
