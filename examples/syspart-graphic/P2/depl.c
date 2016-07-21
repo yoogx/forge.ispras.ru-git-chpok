5972b8d80cf8004aad8884fcdf88b8e18f5a3e00
Kurban Mallachiev <mallachiev@ispras.ru>
2016-07-21 14:23:43 +0300

    add configuration of BAR0 and BAR2 of radeon pci card
    u-boot sets correct values of BAR0 and BAR2. This commit just sets
    corresponding virtual address

--- examples/syspart-graphic/P2/depl.c
+++ examples/syspart-graphic/P2/depl.c
@@ -53,6 +53,16 @@ struct pci_dev_config pci_configs[] = {
         .dev = 0,
         .fn  = 0,
         .resources = {
+            [PCI_RESOURCE_BAR0] = {
+                .addr = 0x20000000,
+                .pci_addr = 0, //leave unmodified (as set by u-boot)
+                .type = PCI_RESOURCE_TYPE_BAR_MEM
+            },
+            [PCI_RESOURCE_BAR2] = {
+                .addr = 0x30000000,
+                .pci_addr = 0, //leave unmodified (as set by u-boot)
+                .type = PCI_RESOURCE_TYPE_BAR_MEM
+            },
             [PCI_RESOURCE_ROM] = {
                 .addr =     0x30020000,
                 .pci_addr = 0x90020000,
