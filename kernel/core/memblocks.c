f283f77bd2fda4d01299962d00d11385d459562b
Kurban Mallachiev <mallachiev@ispras.ru>
2016-08-12 17:43:12 +0300

    pok_memory_block_get_status: print current parition pid

--- kernel/core/memblocks.c
+++ kernel/core/memblocks.c
@@ -1,10 +1,13 @@
 #include <core/memblocks.h>
 #include <libc.h>
+#include <core/partition.h>
 pok_ret_t pok_memory_block_get_status(
         const char* name,
         jet_memory_block_status_t* __user status)
 {
+    int current_pid = current_partition->space_id + 1;
     printf("%s %s\n", __func__, name);
+    printf("partition space_id = %d\n", current_pid);
 
     status->addr = 0xed000000;
     status->size = 65536;
