553ae6a93d2a448ae3131c33764b20196614b568
Kurban Mallachiev <mallachiev@ispras.ru>
2016-09-01 15:31:37 +0300

    minor code rearange in udp_ip_send

--- syspart/net/udp_ip_send.c
+++ syspart/net/udp_ip_send.c
@@ -29,42 +29,41 @@
 #include "net.h"
 
 #define UDP_IP_HEADER_SIZE (20+8)
+struct udp_ip_packet{
+    struct ip_hdr ip_hdr;
+    struct udp_hdr udp_hdr;
+    char payload[];
+} __attribute__((packed));
 
 static void fill_in_udp_ip_header(
-        char *buffer,
+        struct udp_ip_packet *packet,
         size_t size, // size of UDP data
         uint32_t dst_ip,
         uint16_t dst_port)
 {
-    struct {
-        struct ip_hdr ip_hdr;
-        struct udp_hdr udp_hdr;
-        char data[];
-    } __attribute__((packed)) *real_buffer = (void*)buffer;
-
     // ...next, IP heaader
-    real_buffer->ip_hdr.version_len = (4 << 4) | 5;
-    real_buffer->ip_hdr.dscp = 0;
-    real_buffer->ip_hdr.length = hton16(
+    packet->ip_hdr.version_len = (4 << 4) | 5;
+    packet->ip_hdr.dscp = 0;
+    packet->ip_hdr.length = hton16(
             sizeof(struct ip_hdr) +
             sizeof(struct udp_hdr) +
             size
     );
-    real_buffer->ip_hdr.checksum = 0; // it's filled in just below
-    real_buffer->ip_hdr.id = 0;
-    real_buffer->ip_hdr.offset = 0;
-    real_buffer->ip_hdr.ttl = 32;
-    real_buffer->ip_hdr.proto = IPPROTO_UDP;
-    real_buffer->ip_hdr.src = hton32(pok_network_ip_address);
-    real_buffer->ip_hdr.dst = hton32(dst_ip);
+    packet->ip_hdr.checksum = 0; // it's filled in just below
+    packet->ip_hdr.id = 0;
+    packet->ip_hdr.offset = 0;
+    packet->ip_hdr.ttl = 32;
+    packet->ip_hdr.proto = IPPROTO_UDP;
+    packet->ip_hdr.src = hton32(pok_network_ip_address);
+    packet->ip_hdr.dst = hton32(dst_ip);
 
-    real_buffer->ip_hdr.checksum = ip_hdr_checksum(&real_buffer->ip_hdr);
+    packet->ip_hdr.checksum = ip_hdr_checksum(&packet->ip_hdr);
 
     // ... and UDP header
-    real_buffer->udp_hdr.src_port = hton16(dst_port);
-    real_buffer->udp_hdr.dst_port = hton16(dst_port);
-    real_buffer->udp_hdr.length = hton16(size + sizeof(struct udp_hdr));
-    real_buffer->udp_hdr.checksum = 0; // no checksum
+    packet->udp_hdr.src_port = hton16(dst_port);
+    packet->udp_hdr.dst_port = hton16(dst_port);
+    packet->udp_hdr.length = hton16(size + sizeof(struct udp_hdr));
+    packet->udp_hdr.checksum = 0; // no checksum
 }
 
 pok_bool_t udp_ip_send(
