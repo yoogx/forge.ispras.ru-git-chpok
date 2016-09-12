76c3d83e01c5da335ab77660e710482cfed31913
Kurban Mallachiev <mallachiev@ispras.ru>
2016-09-12 11:14:47 +0300

    change name of X to X1 and add X2 to config file for debug

--- syspart/components/X/x_gen.c
+++ syspart/components/X/x_gen.c
@@ -4,7 +4,7 @@
  * Instead of modifying this file, modify the one it generated from (syspart/components/X/config.yaml).
  */
 #include <lib/common.h>
-#include "x_gen.h"
+#include "x1_gen.h"
 
 
     static void __wrapper_x_send(self_t *arg0
@@ -12,17 +12,17 @@
         ,size_t arg2
     )
     {
-        return x_send((X*) arg0, arg1, arg2);
+        return x_send((X1*) arg0, arg1, arg2);
     }
 
     static void __wrapper_x_flush(self_t *arg0
     )
     {
-        return x_flush((X*) arg0);
+        return x_flush((X1*) arg0);
     }
 
 
-void __X_init__(X *self)
+void __X1_init__(X1 *self)
 {
             self->in.portC.ops.send = __wrapper_x_send;
             self->in.portC.ops.flush = __wrapper_x_flush;
--- syspart/components/X/x_gen.h
+++ syspart/components/X/x_gen.h
@@ -3,19 +3,19 @@
  *
  * Instead of modifying this file, modify the one it generated from (syspart/components/X/config.yaml).
  */
-#ifndef __X_GEN_H__
-#define __X_GEN_H__
+#ifndef __X1_GEN_H__
+#define __X1_GEN_H__
 
 
     #include <interfaces/send_flush_t_gen.h>
 
 
-struct X_state {
+struct X1_state {
         char x;
 };
 
 typedef struct {
-    struct X_state state;
+    struct X1_state state;
     struct {
             struct {
                 send_flush_t ops;
@@ -23,17 +23,17 @@ typedef struct {
     } in;
     struct {
     } out;
-} X;
+} X1;
 
 
-      void x_send(X *
+      void x_send(X1 *
 , void *, size_t);
-      void x_flush(X *
+      void x_flush(X1 *
 );
 
 
 
 
-    void x_init(X *);
+    void x_init(X1 *);
 
 #endif
