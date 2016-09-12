ddf36d8eaf2ef145d1e2e53623a4f0f85acac306
Kurban Mallachiev <mallachiev@ispras.ru>
2016-09-12 11:58:34 +0300

    correctly handle several components in one directory

---
+++ syspart/components/X/X2_gen.c
@@ -0,0 +1,14 @@
+/*
+ * GENERATED! DO NOT MODIFY!
+ *
+ * Instead of modifying this file, modify the one it generated from (syspart/components/X/config.yaml).
+ */
+#include <lib/common.h>
+#include "x2_gen.h"
+
+
+
+void __X2_init__(X2 *self)
+{
+
+}
---
+++ syspart/components/X/X2_gen.h
@@ -0,0 +1,30 @@
+/*
+ * GENERATED! DO NOT MODIFY!
+ *
+ * Instead of modifying this file, modify the one it generated from (syspart/components/X/config.yaml).
+ */
+#ifndef __X2_GEN_H__
+#define __X2_GEN_H__
+
+
+
+
+struct X2_state {
+        int tmp;
+};
+
+typedef struct {
+    struct X2_state state;
+    struct {
+    } in;
+    struct {
+    } out;
+} X2;
+
+
+
+
+
+
+
+#endif
