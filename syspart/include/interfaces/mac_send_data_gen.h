a8a9d92ab4a4991891c656f8a0c1ac22009406f8
Kurban Mallachiev <mallachiev@ispras.ru>
2016-09-16 18:46:24 +0300

    change port type betwen UPD_IP and MAC

---
+++ syspart/include/interfaces/mac_send_data_gen.h
@@ -0,0 +1,20 @@
+/*
+ * GENERATED! DO NOT MODIFY!
+ *
+ * Instead of modifying this file, modify the one it generated from (syspart/include/interfaces/send_net.yaml).
+ */
+#ifndef __INTERFACES_MAC_SEND_DATA_H__
+#define __INTERFACES_MAC_SEND_DATA_H__
+
+#include <lib/common.h>
+    #include <ret_type.h>
+    #include <net/ether.h>
+
+typedef struct {
+    ret_t (*mac_send)(self_t *, char *, size_t, size_t, enum ethertype);
+    ret_t (*flush)(self_t *);
+} mac_send_data;
+
+
+#endif
+
