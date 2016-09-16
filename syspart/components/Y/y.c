52077050a670c06604f0643944732b24a3a31ffa
Kurban Mallachiev <mallachiev@ispras.ru>
2016-09-16 16:01:13 +0300

    update components to use new call_port_function functions

--- syspart/components/Y/y.c
+++ syspart/components/Y/y.c
@@ -5,9 +5,10 @@ void y_tick(Y *self)
 {
     printf("Y<y=%d>.tick()\n", self->state.y);
 
-    CALL_PORT_FUNCTION(self, portB, send, NULL, 101);
+    //CALL_PORT_FUNCTION(self, portB, send, NULL, 0x10);
+    call_portB_send(self, NULL, 0x10);
 
-    CALL_PORT_FUNCTION(self, portB, flush);
+    call_portB_flush(self);
 
 }
 
