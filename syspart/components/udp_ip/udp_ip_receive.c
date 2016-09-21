d89b62d8450594b75c02097178380249155809f4
Kurban Mallachiev <mallachiev@ispras.ru>
2016-09-21 14:03:12 +0300

    UDP_RECEIVER add debug print and fir warnings

--- syspart/components/udp_ip/udp_ip_receive.c
+++ syspart/components/udp_ip/udp_ip_receive.c
@@ -29,6 +29,8 @@
 #include "net.h"
 #include "UDP_RECEIVER_FILTER_gen.h"
 
+#define C_NAME "UDP_RECEIVER_FILTER: "
+
 #define UDP_IP_HEADER_SIZE (20+8)
 struct udp_ip_packet{
     struct ip_hdr ip_hdr;
@@ -38,20 +40,19 @@ struct udp_ip_packet{
 
 ret_t udp_receive_and_filter(UDP_RECEIVER_FILTER *self, char *data, size_t len)
 {
-    printf("OOOOOOOOOOOOOOOOO\n");
     const struct ip_hdr *ip_hdr = (const struct ip_hdr *) data;
 
     if (len < sizeof(struct ip_hdr) || len < (size_t) (ip_hdr->version_len & 0xf) * 4) {
-        printf("Received packet is too small (IP header doesn't fit).\n");
-        return;
+        printf(C_NAME"Received packet is too small (IP header doesn't fit).\n");
+        return EINVAL;
     }
 
     if (len != ntoh16(ip_hdr->length)) {
         if (len > ntoh16(ip_hdr->length)) {
             len = ntoh16(ip_hdr->length);
         } else {
-            printf("Packet length mismatch (received buffer size vs. specified in IP header).\n");
-            return;
+            printf(C_NAME"Packet length mismatch (received buffer size vs. specified in IP header).\n");
+            return EINVAL;
         }
     }
 
@@ -59,8 +60,8 @@ ret_t udp_receive_and_filter(UDP_RECEIVER_FILTER *self, char *data, size_t len)
     len -= (ip_hdr->version_len & 0xf) * 4;
 
     if (ip_hdr_checksum(ip_hdr) != 0) {
-        printf("Discarded IP packet with incorrect header checksum.\n");
-        return;
+        printf(C_NAME"Discarded IP packet with incorrect header checksum.\n");
+        return EINVAL;
     }
 
     // TODO network broadcast address?
@@ -68,31 +69,30 @@ ret_t udp_receive_and_filter(UDP_RECEIVER_FILTER *self, char *data, size_t len)
         ip_hdr->dst != hton32(pok_network_ip_address))
     {
         // it's not for us
-        printf("not for us %x\n", ip_hdr->dst);
-        return;
+        printf(C_NAME"not for us %lx\n", ip_hdr->dst);
+        return EINVAL;
     }
 
     if (ip_hdr->proto != IPPROTO_UDP) {
         // we don't know anything except UDP
-        return;
+        return EINVAL;
     }
 
     if (len < sizeof(struct udp_hdr)) {
-        printf("Received IP packet is too small (UDP header doesn't fit).\n");
-        return;
+        printf(C_NAME"Received IP packet is too small (UDP header doesn't fit).\n");
+        return EINVAL;
     }
 
     const struct udp_hdr *udp_hdr = (const struct udp_hdr *) data;
 
     if (ntoh16(udp_hdr->length) != len) {
-        printf("Packet length mismatch (received buffer size vs. specified in UDP header).\n");
-        return;
+        printf(C_NAME"Packet length mismatch (received buffer size vs. specified in UDP header).\n");
+        return EINVAL;
     }
 
-    data += sizeof(struct udp_hdr);
-    len -= sizeof(struct udp_hdr);
-
-    printf("GOOD UDP PACKET\n");
+    printf(C_NAME"GOOD UDP PACKET\n");
+    hexdump(udp_hdr->payload, len-sizeof(struct udp_hdr));
+    return EOK;
     //received_callback(
     //        ntoh32(ip_hdr->dst),
     //        ntoh16(udp_hdr->dst_port),
