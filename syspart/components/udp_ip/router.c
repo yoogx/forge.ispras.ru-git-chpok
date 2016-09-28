d293d4a8691e36f674276af8f07fbaecacd221dc
Kurban Mallachiev <mallachiev@ispras.ru>
2016-09-28 16:42:53 +0300

    update ROUTER to use array of ports

--- syspart/components/udp_ip/router.c
+++ syspart/components/udp_ip/router.c
@@ -22,13 +22,29 @@
 
 #define C_NAME "ROUTER: "
 
+/* Return index of (ip, port) pair in array in state. -1 if not found */
+static int get_ip_port_index(ROUTER_state *state, uint32_t ip, uint16_t port)
+{
+    for (int i=0; i<state->map_ip_port_to_idx_len; i++) {
+        struct udp_ip_pair *cur_pair = &state->map_ip_port_to_idx[i];
+        if (cur_pair->ip == ip && cur_pair->port == port)
+            return i;
+    }
+    return -1;
+}
+
 ret_t receive_packet(ROUTER *self, char *payload, size_t payload_size, uint32_t ip, uint16_t port)
 {
-    printf(C_NAME"packet from %ld.%ld.%ld.%ld:%d\n", IP_PRINT(ip), port);
     printf(C_NAME"GOOD UDP PACKET\n");
     hexdump(payload, payload_size);
 
-    ROUTER_call_portArray_send_by_index(1, self, payload, payload_size);
+    int idx = get_ip_port_index(&self->state, ip, port);
+
+    if (idx < 0) {
+        printf(C_NAME"packet not for us (from %ld.%ld.%ld.%ld:%d)\n", IP_PRINT(ip), port);
+        return EINVAL;
+    }
+    ROUTER_call_portArray_send_by_index(idx, self, payload, payload_size);
 
     return EOK;
 }
