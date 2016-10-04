9a2f99ccac57fc8ab660a9c2eea72be11b308415
Kurban Mallachiev <mallachiev@ispras.ru>
2016-10-04 17:32:31 +0300

    delete debug X and Y components

--- examples/syspart-network-queue-nc/P2/components-glue/glue_main.c
+++ examples/syspart-network-queue-nc/P2/components-glue/glue_main.c
@@ -1,48 +1,6 @@
 
 #include <stdio.h>
 
-#include <X_gen.h>
-#include <Y_gen.h>
-/* _________                 _________
-   \ portA /                 \ portA /
-    \     /                   \     /
-     \   /                     \   /
- _____\_/_____             _____\_/_____
-|             |           |             |
-|     Y1      |           |     Y2      |
-|   _______   |           |   _______   |
-|    y = 1    |           |    y = 2    |
-|_____________|           |_____________|
-   \ portB /                 \ portB /
-    \     /                   \     /
-     \   /                     \   /
-      \ /                       \ /
-       |                         |
-       |                         |
-       |                         |
-       |                         |
-   ___\_/___                 ___\_/___
-   \ portC /                 \ portC /
-    \     /                   \     /
-     \   /                     \   /
- _____\_/____              _____\_/____
-|            |            |            |
-|     X1     |            |     X2     |
-|   _______  |            |   _______  |
-|    x = 1   |            |    x = 2   |
-|____________|            |____________|
-
-
-portA: tick_t(tick)
-portB, portC: send_flush_t(send, flush)
-*/
-
-extern Y y_1;
-extern Y y_2;
-void __components_init__();
-void __components_activity__();
-void y_tick(Y *);
-
 void glue_activity()
 {
     __components_activity__();
@@ -51,8 +9,4 @@ void glue_activity()
 void glue_main()
 {
     __components_init__();
-    printf("\n");
-    y_tick(&y_1);
-    printf("\n");
-    y_tick(&y_2);
 }
--- examples/syspart-network-queue-nc/P2/components-glue/glue_main_gen.c
+++ examples/syspart-network-queue-nc/P2/components-glue/glue_main_gen.c
@@ -8,42 +8,6 @@ struct port_ops{
     void *owner;
 };
 
-    #include <X_gen.h>
-        void __X_init__(X*);
-        void __X_activity__(X*);
-        X x_1 = {
-            .state = {
-                .x = 1,
-            },
-
-        };
-        void __X_init__(X*);
-        void __X_activity__(X*);
-        X x_2 = {
-            .state = {
-                .x = 2,
-            },
-
-        };
-
-    #include <Y_gen.h>
-        void __Y_init__(Y*);
-        void __Y_activity__(Y*);
-        Y y_1 = {
-            .state = {
-                .y = 1,
-            },
-
-        };
-        void __Y_init__(Y*);
-        void __Y_activity__(Y*);
-        Y y_2 = {
-            .state = {
-                .y = 2,
-            },
-
-        };
-
     #include <ARINC_SENDER_gen.h>
         void __ARINC_SENDER_init__(ARINC_SENDER*);
         void __ARINC_SENDER_activity__(ARINC_SENDER*);
@@ -207,12 +171,6 @@ struct port_ops{
 
 void __components_init__()
 {
-            __X_init__(&x_1);
-            __X_init__(&x_2);
-
-            __Y_init__(&y_1);
-            __Y_init__(&y_2);
-
             __ARINC_SENDER_init__(&arinc_sender_1);
 
             __UDP_IP_SENDER_init__(&udp_ip_sender_1);
@@ -242,10 +200,6 @@ void __components_init__()
             __ARINC_RECEIVER_init__(&arinc_receiver_2);
 
 
-        y_1.out.portB.ops = &x_1.in.portC.ops;
-        y_1.out.portB.owner = &x_1;
-        y_2.out.portB.ops = &x_2.in.portC.ops;
-        y_2.out.portB.owner = &x_2;
         arinc_sender_1.out.portA.ops = &udp_ip_sender_1.in.portA.ops;
         arinc_sender_1.out.portA.owner = &udp_ip_sender_1;
         udp_ip_sender_1.out.portB.ops = &mac_sender_1.in.portA.ops;
@@ -282,10 +236,6 @@ void __components_init__()
 void __components_activity__()
 {
     while (1) {
-                __X_activity__(&x_1);
-                __X_activity__(&x_2);
-                __Y_activity__(&y_1);
-                __Y_activity__(&y_2);
                 __ARINC_SENDER_activity__(&arinc_sender_1);
                 __UDP_IP_SENDER_activity__(&udp_ip_sender_1);
                 __MAC_SENDER_activity__(&mac_sender_1);
