328ffebe57b7995ab7ecec38a0212fba68c17f99
Kurban Mallachiev <mallachiev@ispras.ru>
2017-01-30 17:17:53 +0300

    add cpu_config.h for p3041 bsp

---
+++ boards/p3041/include/board/cpu_config.h
@@ -0,0 +1,21 @@
+/*
+ * Institute for System Programming of the Russian Academy of Sciences
+ * Copyright (C) 2016 ISPRAS
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, Version 3.
+ *
+ * This program is distributed in the hope # that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License version 3 for more details.
+ */
+
+#ifndef __JET_P3041_CPU_CONFIG_H__
+#define __JET_P3041_CPU_CONFIG_H__
+
+#define JET_PPC_CONFIG_E500MC
+
+#endif /*__JET_E500_HIGHMEM_CPU_CONFIG_H__*/
