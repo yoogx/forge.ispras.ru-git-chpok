cdd69a617de387aaeeb75c99da050b6f0c2d7d8e
Kurban Mallachiev <mallachiev@ispras.ru>
2017-01-31 13:14:56 +0300

    dstec: add check if __PPC__ defined.
    This allows correctly handle compilation for other archs

--- syspart/components/ppc_dtsec/main.c
+++ syspart/components/ppc_dtsec/main.c
@@ -7,9 +7,11 @@ int fm_eth_recv(DTSEC_NET_DEV *self);
 
 ret_t dtsec_send_frame(DTSEC_NET_DEV *self, char *buffer, size_t size, size_t max_back_step)
 {
+#ifdef __PPC__
     printf("DTSEC %s\n", __func__);
 
     fm_eth_send(self->state.dev_state.current_fm, buffer, size);
+#endif
     return 0;
 }
 
@@ -22,11 +24,18 @@ ret_t dtsec_flush_send(DTSEC_NET_DEV *self)
 
 void dtsec_component_init(DTSEC_NET_DEV *self)
 {
+
+#ifdef __PPC__
     dtsec_init(self);
     printf("DTSEC init\n");
+#else
+    printf("ERROR: DTSEC doesn't support archectures other than PPC\n");
+#endif
 }
 
 void dtsec_receive_activity(DTSEC_NET_DEV *self)
 {
+#ifdef __PPC__
     fm_eth_recv(self);
+#endif
 }
