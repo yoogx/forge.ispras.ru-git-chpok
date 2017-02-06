f6dda57a01e3b43d14bb9c66300606a9bd2f4798
Kurban Mallachiev <mallachiev@ispras.ru>
2017-02-06 16:28:54 +0300

    implement simple imx6 UART driver

--- kernel/arch/arm/include/bsp/ioports.h
+++ kernel/arch/arm/include/bsp/ioports.h
@@ -9,3 +9,23 @@ static inline uint32_t ioread32(uintptr_t reg)
 {
     return *(volatile uint32_t *)reg;
 }
+
+static inline void iowrite16(uintptr_t reg, uint16_t data)
+{
+    *(volatile uint16_t *)reg = data;
+}
+
+static inline uint16_t ioread16(uintptr_t reg)
+{
+    return *(volatile uint16_t *)reg;
+}
+
+static inline void iowrite8(uintptr_t reg, uint8_t data)
+{
+    *(volatile uint8_t *)reg = data;
+}
+
+static inline uint8_t ioread8(uintptr_t reg)
+{
+    return *(volatile uint8_t *)reg;
+}
