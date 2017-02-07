1ab9da6749006ea4576e951879367d213894e68a
Kurban Mallachiev <mallachiev@ispras.ru>
2017-02-07 15:38:35 +0300

    finish merging with develop

--- kernel/arch/arm/space.c
+++ kernel/arch/arm/space.c
@@ -16,33 +16,37 @@
 #include <asp/space.h>
 #include <assert.h>
 
-void ja_space_layout_get(jet_space_id space_id,
-    struct jet_space_layout* space_layout)
+void ja_uspace_grant_access(void)
+{
+    assert(0);
+}
+
+void ja_uspace_revoke_access(void)
+{
+    assert(0);
+}
+
+void ja_uspace_grant_access_local(jet_space_id space_id)
+{
+    assert(0);
+}
+
+void ja_uspace_revoke_access_local(jet_space_id space_id)
 {
     assert(0);
 }
 
 
-void* ja_space_get_heap(jet_space_id space_id)
-{
-    assert(0);
-}
-
-struct jet_kernel_shared_data* __kuser ja_space_shared_data(jet_space_id space_id)
-{
-    assert(0);
-}
-
-/**
+/*
  * Jump to the user space.
- * 
+ *
  * Kernel stack passed as 'sp' will be used in interrupts/syscalls.
  */
 void ja_user_space_jump(
     jet_stack_t stack_kernel,
     jet_space_id space_id, /* Actually, unused (should already be set with ja_space_switch). */
     void (__user * entry_user)(void),
-    jet_ustack_t stack_user)
+    uintptr_t stack_user)
 {
     assert(0);
 }
@@ -57,12 +61,7 @@ jet_space_id ja_space_get_current (void)
     assert(0);
 }
 
-void ja_ustack_init (jet_space_id space_id)
-{
-    assert(0);
-}
-
-jet_ustack_t ja_ustack_alloc (jet_space_id space_id, size_t stack_size)
+size_t ja_ustack_get_alignment(void)
 {
     assert(0);
 }
