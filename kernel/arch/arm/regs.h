78e29601b4ec5d72f4c6b4e1c9e40b1b5e61e2b5
Kurban Mallachiev <mallachiev@ispras.ru>
2017-03-09 15:10:37 +0300

    add cpsrs modes to regs.h

--- kernel/arch/arm/regs.h
+++ kernel/arch/arm/regs.h
@@ -16,6 +16,7 @@
 #ifndef __ARM_REGS_H__
 #define __ARM_REGS_H__
 
+#ifndef __ASSEMBLER__
 static inline uint32_t cpsrget()
 {
     uint32_t r;
@@ -27,7 +28,18 @@ static inline void cpsrset(uint32_t r)
 {
     asm("msr cpsr, %0" : : "r" (r));
 }
+#endif // __ASSEMBLER__
 
 #define CPSR_IRQ (1<<7)
+
+#define CPSR_MODE_USR 0x10 //User
+#define CPSR_MODE_FIQ 0x11 //Fast IRQ
+#define CPSR_MODE_IRQ 0x12 // IRQ
+#define CPSR_MODE_SVC 0x13 // Supervisor
+#define CPSR_MODE_MON 0x16 // Monitor
+#define CPSR_MODE_ABT 0x17 // Abort
+#define CPSR_MODE_HYP 0x1a // Hyp
+#define CPSR_MODE_UND 0x1b // Undef
+#define CPSR_MODE_SYS 0x1f // System
 
 #endif //__ARM_REGS_H__
