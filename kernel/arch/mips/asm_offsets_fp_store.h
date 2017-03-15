60b55224005412d6e85276418ba18658ccb6097a
Alexander Emelenko <emelenko@ispras.ru>
2017-03-15 14:42:39 +0300

    Fix for fp registers - now we save all cp1 register during context switch

---
+++ kernel/arch/mips/asm_offsets_fp_store.h
@@ -0,0 +1,26 @@
+/*
+ * GENERATED! DO NOT MODIFY!
+ *
+ * Instead of modifying this file, modify the one it generated from (kernel/arch/ppc/asm_offsets_fp_store.def).
+ */
+/*
+ * Institute for System Programming of the Russian Academy of Sciences
+ * Copyright (C) 2016 ISPRAS
+ *
+ * This program is free software) you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, Version 3.
+ *
+ * This program is distributed in the hope # that it will be useful,
+ * but WITHOUT ANY WARRANTY) without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License version 3 for more details.
+ */
+
+/* Fields of jet_fp_store structure. */
+#define OFFSETOF_jet_fp_store_fp_fir 256
+#define OFFSETOF_jet_fp_store_fp_fconfig 260
+#define OFFSETOF_jet_fp_store_fp_fcsr 264
+#define OFFSETOF_jet_fp_store_fp_regs 0
+
