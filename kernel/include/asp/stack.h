a77739fa6d0e21f66bf1b46d7fa9110699f25a02
Kurban Mallachiev <mallachiev@ispras.ru>
2017-03-23 15:53:41 +0300

    arch no longer should define jet_stack_t type

--- kernel/include/asp/stack.h
+++ kernel/include/asp/stack.h
@@ -18,18 +18,19 @@
 #ifndef __JET_ASP_STACK_H__
 #define __JET_ASP_STACK_H__
 
-/* 
- * Arch header should define type 'jet_stack_t' as pointer to allocated
- * (kernel) stack.
+
+#include <stdint.h>
+/*
+ * pointer to allocated * (kernel) stack.
  */
-#include <arch/stack.h>
+typedef uintptr_t jet_stack_t;
 
 /**
  * Alloc (kernel) stack of specified size.
- * 
+ *
  * Return pointer to the head of the stack, that is when stack is assumed
  * to be empty.
- * 
+ *
  * May be called only during OS initialization.
  */
 jet_stack_t pok_stack_alloc(uint32_t stack_size);
