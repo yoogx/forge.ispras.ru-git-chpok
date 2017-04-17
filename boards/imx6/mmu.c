c71f2c2a1024af242dcc72418a1155ee29ff77cf
Kurban Mallachiev <mallachiev@ispras.ru>
2017-04-17 11:56:29 +0300

    arm: imx6: add RO access flag to vector table during "normal" execution

--- boards/imx6/mmu.c
+++ boards/imx6/mmu.c
@@ -22,7 +22,12 @@
 //TODO check alignment
 
 // 1KB aligned
-__attribute__ ((aligned(L2_TABLE_SIZE))) uint32_t vector_l2_table[L2_TABLE_SIZE] = {
+__attribute__ ((aligned(L2_TABLE_SIZE))) uint32_t vector_l2_table_rw[L2_TABLE_SIZE] = {
+    [L2_IDX(VECTOR_HIGH_VADDR)] = (VECTOR_PADDR) | L2_SECT_PRIVILEGED_RW | L2_SECT_MEM_DEFAULT | L2_SECT_NON_SUPER,
+};
+
+// 1KB aligned
+__attribute__ ((aligned(L2_TABLE_SIZE))) uint32_t vector_l2_table_ro[L2_TABLE_SIZE] = {
     [L2_IDX(VECTOR_HIGH_VADDR)] = (VECTOR_PADDR) | L2_SECT_PRIVILEGED_RW | L2_SECT_MEM_DEFAULT | L2_SECT_NON_SUPER,
 };
 
@@ -31,14 +36,15 @@ __attribute__ ((aligned(L2_TABLE_SIZE))) uint32_t vector_l2_table[L2_TABLE_SIZE]
  // and address of vector_l2_table leads to "error: initializer element is not constant"
 __attribute__ ((aligned(L1_TABLE_SIZE))) uint32_t entry_l1_table[L1_TABLE_SIZE] = {
     // vector table
-    [L1_IDX(VECTOR_HIGH_VADDR)] = PHYS(vector_l2_table) + L1_TYPE_TABLE,
+    // Maps page: VA=VECTOR_HIGH_VADDR to PA=VECTOR_PADDR. RW access to allow vector table filling
+    [L1_IDX(VECTOR_HIGH_VADDR)] = PHYS(vector_l2_table_rw) + L1_TYPE_TABLE,
 
     // kernel low address
     // Map VA's [0, 1MB) to PA's [0, 1MB). Hope this will be enough
     [L1_IDX(KERNBASE_PADDR)]= (KERNBASE_PADDR) | L1_SECT_PRIVILEGED_RW | L1_SECT_MEM_DEFAULT | L1_TYPE_SECT,
 
     // kernel high address
-    [L1_IDX(KERNBASE_VADDR)] = (KERNBASE_PADDR) | L1_SECT_PRIVILEGED_RW | L1_SECT_MEM_DEFAULT | L1_TYPE_SECT,
+    [L1_IDX(KERNBASE_VADDR)]     = (KERNBASE_PADDR) | L1_SECT_PRIVILEGED_RW | L1_SECT_MEM_DEFAULT | L1_TYPE_SECT,
     [L1_IDX(KERNBASE_VADDR) + 1] = (KERNBASE_PADDR + (1<<L1_IDX_SHIFT)) | L1_SECT_PRIVILEGED_RW | L1_SECT_MEM_DEFAULT | L1_TYPE_SECT,
     [L1_IDX(KERNBASE_VADDR) + 2] = (KERNBASE_PADDR + (2<<L1_IDX_SHIFT)) | L1_SECT_PRIVILEGED_RW | L1_SECT_MEM_DEFAULT | L1_TYPE_SECT,
     [L1_IDX(KERNBASE_VADDR) + 3] = (KERNBASE_PADDR + (3<<L1_IDX_SHIFT)) | L1_SECT_PRIVILEGED_RW | L1_SECT_MEM_DEFAULT | L1_TYPE_SECT,
@@ -54,8 +60,9 @@ __attribute__ ((aligned(L1_TABLE_SIZE))) uint32_t entry_l1_table[L1_TABLE_SIZE] 
 
 void insert_kernel_mapping_into_table(uint32_t *l1_table)
 {
-    //vector_table
-    l1_table[L1_IDX(VECTOR_HIGH_VADDR)] = PHYS(vector_l2_table) + L1_TYPE_TABLE;
+    // vector_table
+    // Maps page: VA=VECTOR_HIGH_VADDR to PA=VECTOR_PADDR. RO access to not allow changing
+    l1_table[L1_IDX(VECTOR_HIGH_VADDR)] = PHYS(vector_l2_table_ro) + L1_TYPE_TABLE;
 
     // kernel high address
     l1_table[L1_IDX(KERNBASE_VADDR)]     = (KERNBASE_PADDR) | L1_SECT_PRIVILEGED_RW | L1_SECT_MEM_DEFAULT | L1_TYPE_SECT;
